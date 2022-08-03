dd13ecfe83238e6ff28e1d033ea78d3ccc525fd8
yeonji <view7186@naver.com>
2022-08-03 17:22:00 +0900

    Make function of setting output_dir

--- create-superstar-dataset/ss_to_ds_json_converter.py
+++ create-superstar-dataset/ss_to_ds_json_converter.py
@@ -4,7 +4,10 @@ from glob import glob
 OUTPUT_DIR = "./ssdsconverter_output/"
 
 class SSDSJsonConverter():
-    def __init__(self, output_path):
+    def __init__(self):
+        pass
+
+    def set_output_path(self, output_path):
         self.output_path = output_path
         if not os.path.isdir(self.output_path):
             os.makedirs(self.output_path)
@@ -26,9 +29,8 @@ class SSDSJsonConverter():
         output_file.close()
 
 if __name__ == '__main__':
-    
-    converter = SSDSJsonConverter(OUTPUT_DIR)
-
+    converter = SSDSJsonConverter()
+    converter.set_output_path(OUTPUT_DIR)
     json_dir = glob("./SuperStarResource/json/*")
     for gametype_dir in json_dir:
         json_path_list = glob(gametype_dir+"/*.json")
