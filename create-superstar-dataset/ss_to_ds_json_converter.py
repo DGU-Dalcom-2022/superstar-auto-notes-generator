2ca3e35c079dcc3a771315dee96e389733759d34
yeonji <view7186@naver.com>
2022-08-05 14:16:12 +0900

    Exception process & Modify value and main part - 현재 json의 타이틀이 다르게 설정되어서 난이도를 인식할 수 없는 경우가 있음 -> 예외처리 - input_path는 self로 하지 않게 함 - main 부분 OUTPUT_DIR 관련해서 변경

--- create-superstar-dataset/ss_to_ds_json_converter.py
+++ create-superstar-dataset/ss_to_ds_json_converter.py
@@ -1,22 +1,35 @@
 import json
 import os
 from glob import glob
-OUTPUT_DIR = "./ssdsconverter_output/"
+OUTPUT_DIR = "./ssdsconverter_output"
 
 class SSDSJsonConverter():
     def __init__(self):
+        print('새로운 converter 생성됨. 이름 마지막에 4, 7, 13으로 난이도 설정이 되어 있지 않은 파일은 생성 불가')
         pass
 
     def set_output_path(self, output_path):
         self.output_path = output_path
         if not os.path.isdir(self.output_path):
             os.makedirs(self.output_path)
-    
-    def convert(self, input_path):
-        self.input_path = input_path
-        output_file = open(self.output_path+self.input_path.split('/')[-1], 'w')
 
-        old_notes = json.load(open(self.input_path.__str__(), "r"))["notes"]
+    def get_difficulty(self, song_idx_num):
+        return {'4' : 'Easy.dat', '7' : 'Normal.dat', '13' : 'Hard.dat'}.get(song_idx_num, '-1')
+    
+    def convert(self, input_path, output_path):
+        self.set_output_path(output_path)
+
+        song_title = input_path.split('/')[-1].split('_')[:-1]
+        song_idx_num = input_path.split('/')[-1].split('_')[-1].split('.')[0]
+        song_title = '_'.join(song_title)
+        song_diff = self.get_difficulty(song_idx_num)
+        if song_diff == '-1':
+            return print(input_path + ' : 잘못된 난이도 설정')
+
+        self.set_output_path(self.output_path+'/'+song_title)
+        output_file = open(self.output_path+'/'+song_diff, 'w')
+
+        old_notes = json.load(open(input_path.__str__(), "r"))["notes"]
         new_note_list = []
         for idx in old_notes:
             for old_note in old_notes[idx]:
@@ -30,9 +43,8 @@ class SSDSJsonConverter():
 
 if __name__ == '__main__':
     converter = SSDSJsonConverter()
-    converter.set_output_path(OUTPUT_DIR)
-    json_dir = glob("./SuperStarResource/json/*")
-    for gametype_dir in json_dir:
-        json_path_list = glob(gametype_dir+"/*.json")
+    json_dir = glob('../SuperStarResource/json/*')
+    for game_dir in json_dir:
+        json_path_list = glob(game_dir+"/*.json")
         for json_path in json_path_list:
-            converter.convert(json_path)
+            converter.convert(json_path, OUTPUT_DIR+'/'+game_dir.split('/')[-1])
